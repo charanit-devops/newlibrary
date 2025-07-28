db2c0a78912fec0c638a8869ffad29176f389fc2
charanit-devops <95111912+charanit-devops@users.noreply.github.com>
2025-07-28 21:00:30 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,3 +11,8 @@ def deploytoTomcat(jobname,ip,context)
 {
 	sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
+
+def runselenium(jobname)
+{
+	sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
